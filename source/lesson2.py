c0c1f72f218e7a496e6cf21099ecc02ab12702f0
Salsabil18 <101669964+Salsabil18@users.noreply.github.com>
2026-06-27 16:04:15 +0100

    Create lesson2.py

---
+++ source/lesson2.py
@@ -0,0 +1,14 @@
+##### transform image to Grayscale ######
+
+import cv2
+
+image = cv2.imread("document.jpg")
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+## show image ##
+cv2.imshow("Original", image)
+cv2.imshow("Gray", gray)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
