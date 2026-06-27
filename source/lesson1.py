bec80026d5e33f0e8a8857bc5584cc04e090fc68
SilaEngineer <101669964+Salsabil18@users.noreply.github.com>
2026-06-27 17:07:11 +0100

    Enhance comments and modify first pixel of image
    Added comments to clarify image processing steps and modified the first pixel color.

--- source/lesson1.py
+++ source/lesson1.py
@@ -1,7 +1,15 @@
 import cv2
 
-#read the image
-image = cv2.imread("document.jpg")
+image = cv2.imread("document.jpg") # read the image
 
-print(type(image))
-print(image.shape)
+print(type(image)) # print the type of the image object
+
+# print the information of the image   
+print("Height:", image.shape[0])
+print("Width:", image.shape[1])
+print("Channels:", image.shape[2])
+
+image[0,0] = [0, 0, 255] # Change the first pixel at (0,0) to red color  
+
+cv2.imwrite("modified.jpg", image) # save the modified image (the first pixel has been successfully changed to red color )
+
