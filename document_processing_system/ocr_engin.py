0c6f70b9ecb6f1d0caeb71281390a30306e5e182
SilaEngineer <101669964+Salsabil18@users.noreply.github.com>
2026-07-04 23:29:33 +0100

    Implement OCR text extraction using OpenCV and Tesseract

--- document_processing_system/ocr_engin.py
+++ document_processing_system/ocr_engin.py
@@ -1,3 +1,13 @@
-def extract_text(region):
-    ...
+import cv2
+import pytesseract
+
+def extract_text(image_path):
+
+    image=cv2.imread(image_path)
+
+    text=pytesseract.image_to_string(
+        image,
+        lang="ara"
+    )
+
     return text
