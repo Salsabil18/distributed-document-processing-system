e474cf728676cecb4bb70f4eeeb049c3ef0874ee
SilaEngineer <101669964+Salsabil18@users.noreply.github.com>
2026-07-07 22:10:54 +0100

    Add PaddleOCREngine class with unimplemented methods

---
+++ document_processing_system/OCR/paddleocr_engine.py
@@ -0,0 +1,14 @@
+from ocr.base_engine import OCREngine
+
+
+class PaddleOCREngine(OCREngine):
+
+    def __init__(self):
+
+        raise NotImplementedError(
+            "PaddleOCR is not implemented yet."
+        )
+
+    def extract_text(self, image_path):
+
+        pass
